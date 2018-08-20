3119f564e8b54d82709272b1bdb12ee233658a71
Barlowwang <Barlowwang@users.noreply.github.com>
2018-08-20 23:38:34 +0800

    add Insert.hpp

---
+++ cpp_source/Sort/Insert.hpp
@@ -0,0 +1,50 @@
+
+#ifndef INSERT_H
+#define INSERT_H
+
+#include "Sort.hpp"
+#include <stack>
+using namespace std;
+template <class T>
+class Insert final : public Sort<T>
+{
+  public:
+    Insert() : Sort<T>(){};
+    Insert(const vector<T> &data) : Sort<T>(data){};
+    vector<T> method(const vector<T> data, const bool insc) override; ////non-recursive method
+};
+template <typename T>
+vector<T> Insert<T>::method(const vector<T> data, const bool insc)
+{
+    vector<T> tmp = data;
+    T temp = tmp[0];
+    for (int i = 1; i < tmp.size(); i++)  //Iterator is not used here,because insert() and erase() will invalidates the iterator
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (insc)
+                if (tmp[j] > tmp[i])
+                {
+                    temp = tmp[i];
+                    for (int k = i; k >= j; k--)
+                    {
+                        tmp[k] = tmp[k - 1];
+                    }
+                    tmp[j] = temp;
+                    break;
+                }
+                else
+                {
+                    temp = tmp[i];
+                    for (int k = i; k >= j; k--)
+                    {
+                        tmp[k] = tmp[k - 1];
+                    }
+                    tmp[j] = temp;
+                    break;
+                }
+        }
+    }
+    return tmp;
+}
+#endif //INSERT_H
--- cpp_source/Sort/test.cpp
+++ cpp_source/Sort/test.cpp
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <iostream>
 #include <vector>
-#include "Quick.hpp"
+#include "Insert.hpp"
 using namespace std;
 #define random(x) (rand() % x)
 void swap(int &a, int &b)
@@ -11,16 +11,43 @@ void swap(int &a, int &b)
     a = b;
     b = tmp;
 }
+void insertSort(int *a, int length)
+{
+    if (a == NULL)
+        return;
+    int temp = a[0];
+    for (int i = 1; i < length; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (a[j] > a[i])
+            {
+                temp = a[i];
+                for (int k = i; k >= j; k--)
+                {
+                    a[k] = a[k - 1];
+                }
+                a[j] = temp;
+                break;
+            }
+        }
+        cout << i << ": ";
+        for (int hh = 0; hh < length; hh++)
+            cout << a[hh] << " ";
+        cout << endl;
+    }
+}
 int main(int argc, char const *argv[])
 {
-  srand((int)time(NULL));
+    srand((int)time(NULL));
     vector<int> data;
-    int a[10]={50,13,81,8,25,59,52,20,7,19};
+    int a[10] = {50, 13, 81, 8, 25, 59, 52, 20, 7, 19};
     for (int i = 0; i < 10; i++)
     {
-        data.push_back(random(100));
+        // data.push_back(random(100));
+        data.push_back(a[i]);
     }
-   /* for (auto i : data)
+    /* for (auto i : data)
         cout << i << " ";
     cout << endl;
 
@@ -31,12 +58,31 @@ int main(int argc, char const *argv[])
         cout << i << " ";
     cout << endl;
      system("pause");*/
-    Quick<int> b(data);
+    Insert<int> b(data);
     b.showRowData();
     b.sort(true);
     b.showSortedData();
- //   b.sort(false);
-  //  b.showSortedData();
+    b.sort(false);
+    b.showSortedData();
+    /*   for (auto i : data)
+        cout << i << " ";
+    cout << endl;
+    vector<int>::iterator k = data.begin();
+    vector<int>::iterator j = data.begin() + 2;
+    data.insert(k, 20);
+    for (auto i : data)
+        cout << i << " ";
+    cout << endl;
+    data.erase(j+1);
+    for (auto i : data)
+        cout << i << " ";
+    cout << endl;
+    //  insertSort(a, 10);
+    ///   for (auto i : a)
+    ////       cout << i << " ";
+    cout << endl;
+    //   b.sort(false);
+    //  b.showSortedData();*/
     system("pause");
     return 0;
 }
